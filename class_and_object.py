054ed021437fab8be3c8c1c0349f478e64b14a44
rakesh-droid <rakeshleonel@gmail.com>
2020-08-19 22:06:46 +0530

    updated the class and objects files

--- class_and_object.py
+++ class_and_object.py
@@ -1,1 +1,13 @@
-print("hi")
+class mobile:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        print("object is created")
+
+    def dispaly(self):
+        print("Mobile name is: ", self.name)
+        print("Price of the mobile is :", self.price)
+
+m1 = mobile("Iphone", 80000)
+
+m1.dispaly()
